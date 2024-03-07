9c4c9d62948f009707d7ecd21511772c1a96ffeb
Math <math111.0811@gmail.com>
2024-03-07 14:13:12 -0300

    solving the problem with traditional loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        printAllNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
+    }
+
+    private static void printAllNumbers(List<Integer> numbers) {
+        for (int number:numbers) {
+            System.out.println(number);
+        }
+    }
 }
