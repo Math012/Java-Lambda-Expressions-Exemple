ea5678f460009bb40673ba066dfdf578adaa5dcd
Math <math111.0811@gmail.com>
2024-03-08 13:24:40 -0300

    Exercise 3 = Print courses containing the word "Spring"

--- src/main/java/SolvingProblemWithLambda.java
+++ src/main/java/SolvingProblemWithLambda.java
@@ -2,11 +2,12 @@ import java.util.List;
 
 public class SolvingProblemWithLambda {
     public static void main(String[] args) {
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF","Azure","Docker","Kubernetes");
         //printAllNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
         //printEvenNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
         //printOddNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
-        List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF","Azure","Docker","Kubernetes");
-        printAllCourses(courses);
+        //printAllCourses(courses);
+        printSpringCourses(courses);
     }
 
     private static void printOddNumbers(List<Integer> integers) {
@@ -23,6 +24,12 @@ public class SolvingProblemWithLambda {
 
     private static void printAllCourses(List<String> courses) {
         courses.stream().forEach(System.out::println);
+    }
+
+    private static void printSpringCourses(List<String> courses) {
+        courses.stream()
+                .filter(course -> course.startsWith("Spring"))
+                .forEach(System.out::println);
     }
 
 
