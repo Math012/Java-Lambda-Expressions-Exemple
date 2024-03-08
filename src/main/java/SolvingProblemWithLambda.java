fb1d10420b0e099d5ed4d1053b9fb5d68d8e2145
Math <math111.0811@gmail.com>
2024-03-08 13:08:05 -0300

    Exercise 1 = Print only odd numbers from the list

--- src/main/java/SolvingProblemWithLambda.java
+++ src/main/java/SolvingProblemWithLambda.java
@@ -3,7 +3,11 @@ import java.util.List;
 public class SolvingProblemWithLambda {
     public static void main(String[] args) {
         //printAllNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
-        printEvenNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
+        //printEvenNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
+        printOddNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
+    }
+
+    private static void printOddNumbers(List<Integer> integers) {
     }
 
     private static void printAllNumbers(List<Integer> numbers) {
@@ -11,7 +15,7 @@ public class SolvingProblemWithLambda {
     }
 
     private static void printEvenNumbers(List<Integer> numbers) {
-        numbers.stream().filter(number -> number%2==0)
+        numbers.stream().filter(number -> number%2==1)
                 .forEach(System.out::println);
     }
 
