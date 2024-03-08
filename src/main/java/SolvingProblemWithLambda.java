2c182a07fdeba543cb64b6149a83a09977c17c29
Math <math111.0811@gmail.com>
2024-03-08 13:22:33 -0300

    Exercise 2 = Print all courses individually

--- src/main/java/SolvingProblemWithLambda.java
+++ src/main/java/SolvingProblemWithLambda.java
@@ -4,7 +4,9 @@ public class SolvingProblemWithLambda {
     public static void main(String[] args) {
         //printAllNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
         //printEvenNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
-        printOddNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
+        //printOddNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF","Azure","Docker","Kubernetes");
+        printAllCourses(courses);
     }
 
     private static void printOddNumbers(List<Integer> integers) {
@@ -17,6 +19,10 @@ public class SolvingProblemWithLambda {
     private static void printEvenNumbers(List<Integer> numbers) {
         numbers.stream().filter(number -> number%2==1)
                 .forEach(System.out::println);
+    }
+
+    private static void printAllCourses(List<String> courses) {
+        courses.stream().forEach(System.out::println);
     }
 
 
