0eab9e1581542a6a5e36205960e14f0b6b081c4d
Math <math111.0811@gmail.com>
2024-03-08 12:55:34 -0300

    return only even numbers with filter and method reference

--- src/main/java/SolvingProblemWithLambda.java
+++ src/main/java/SolvingProblemWithLambda.java
@@ -2,15 +2,20 @@ import java.util.List;
 
 public class SolvingProblemWithLambda {
     public static void main(String[] args) {
-        printAllNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
+        //printAllNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
+        printEvenNumbers(List.of(1,2,3,4,212,343,53,299,2,3));
     }
 
     private static void printAllNumbers(List<Integer> numbers) {
-        numbers.stream().forEach(SolvingProblemWithLambda::print);
+        numbers.stream().forEach(System.out::println);
     }
 
-    // Creating a method to print each element in our list
-    private static void print(int number){
-        System.out.println(number);
+    private static void printEvenNumbers(List<Integer> numbers) {
+        numbers.stream().filter(SolvingProblemWithLambda::isEven)
+                .forEach(System.out::println);
+    }
+
+    private static boolean isEven(int number){
+        return number%2==0;
     }
 }
